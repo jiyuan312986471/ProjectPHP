6c51eacfa61fb0544b4a9c2d1a4a4e00f6e90387
jiyuan312986471 <jiyuan1005@hotmail.com>
2015-06-01 15:42:05 +0200

    .

--- pers.php
+++ pers.php
@@ -1,8 +1,10 @@
 <?php
+
 require_once '/bdd.php';
 
 
 class personne{
+	
 	public $con;
 	public $id;
 	public $utilisateur;
@@ -10,46 +12,29 @@ class personne{
 	public $conninfo;
 
 
-public function __construct(){
-$this->con=new bdd("RDITS-MDC","bdd_user","user_bdd","ping2");
-$this->conninfo = $this->con->Condb();
-}
-
-  public function login($login, $pass) {
-      
-        $select = " SELECT * FROM [ping2].[dbo].[personne] WHERE [utilisateur] = '%s' AND [passe] = '%s'";
+	public function __construct(){
+		$this->con=new bdd("RDITS-MDC","bdd_user","user_bdd","ping2");
+		$this->conninfo = $this->con->Condb();
+	}
+	
+	public function login($login, $pass) {
+		$select = "SELECT * FROM [ping2].[dbo].[personne] WHERE [utilisateur] = '%s' AND [passe] = '%s'";
 		$fixedlogin = $login;
 		$fixedpass  = $pass;
 		$query = sprintf($select, $fixedlogin, $fixedpass);
 		
-		
 		$result = sqlsrv_query($this->conninfo,$query, array(), array("Scrollable"=>"buffered"));
-
-        if(sqlsrv_num_rows($result) != 1) { 
-
-        	echo "authentification incorrecte";
-
-         }   
-
-        else {
-
-        	$row = sqlsrv_fetch_array($result);
-        	$this->id=$row[0];
-        	$this->utilisateur=$row[1];
-
-        
-		
-		
-     
+	
+	  if(sqlsrv_num_rows($result) != 1) {
+	  	echo "authentification incorrecte";
+	  }   
+		else {
+			$row = sqlsrv_fetch_array($result);
+			$this->id=$row[0];
+			$this->utilisateur=$row[1];     
+		}
+	}
+	
 }
-
-
-    }
-
-
-
-
-}
-
 
 ?>
