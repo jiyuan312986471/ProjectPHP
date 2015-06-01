8495c6c1918abe37fabfaf57020422596754e08a
jiyuan312986471 <jiyuan1005@hotmail.com>
2015-06-01 15:05:21 +0200

    session added

--- trait.php
+++ trait.php
@@ -12,10 +12,13 @@ if(isset($_POST['submit'])){
 	$object->login($name,$pass);
 	
 	if($object->id!=0){
+		// Start Session for user
+		session_start();
+		
+		// Redirection
 		echo"<center><img src=\"img/loading.gif\" /></center>";
-	echo "<meta http-equiv=\"refresh\" content=\"3;dashboard/index.php\" />";
-}
-
+		echo "<meta http-equiv=\"refresh\" content=\"3;dashboard/index.php\" />";
+	}
 }
 
 ?>
