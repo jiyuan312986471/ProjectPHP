9fdd6edcf69ad9e1a1edf3d0383dfc0d1edcfe9c
jiyuan312986471 <jiyuan1005@hotmail.com>
2015-06-10 17:26:20 +0200

    with ajaxPrepareGraphData

--- dashboard/ajaxPrepareGraphData.php
+++ dashboard/ajaxPrepareGraphData.php
@@ -9,14 +9,26 @@
 		case "pourc":
 			// get data
 			$listMachineGraphPourc = $_POST['listMachineGraphPourc'];
+			$graphPourc = $listMachineGraphPourc[$machine];
+			
+			// store as result
+			$result = $graphPourc;
 			break;
 			
 		case "pareto":
+			// get data
+			$listMachineGraphPareto = $_POST['listMachineGraphPareto'];
+			$graphPareto = $listMachineGraphPareto[$machine];
 			
+			// store as result
+			$result = $graphPareto;
 			break;
 			
 		default:
 			break;
 	}
+	
+	// encode result in JSON and send back
+	echo json_encode($result);
 
 ?>
