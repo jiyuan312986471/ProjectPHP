dd761bdf6e7f99b1be7b042df02181d5fa00e946
jiyuan312986471 <jiyuan1005@hotmail.com>
2015-06-15 15:58:24 +0200

    .

--- dashboard/divs/logo.php
+++ dashboard/divs/logo.php
@@ -7,6 +7,5 @@
 	</button>
 	<a class="navbar-brand" href="index.php" style="padding-bottom: 5px">
 		<img src="../img/logo-schneider-electric.png" class="img-responsive" height="125" width="125" style="margin-top: -12px">
-		<!-- <img src="../img/Schneider-Electric-logo-web.png" class="img-responsive" height="100" width="100" style="margin-left: -5px"> -->
 	</a>
 </div>
