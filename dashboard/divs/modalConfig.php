16341e65ca008339352c9ac194b7c18d390ae444
jiyuan312986471 <jiyuan1005@hotmail.com>
2015-06-18 10:04:10 +0200

    .

--- dashboard/divs/modalConfig.php
+++ dashboard/divs/modalConfig.php
@@ -2,13 +2,19 @@
 <div class="modal fade bs-example-modal-lg" id="modalConfig" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
+			
+			<!-- Modal Header -->
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 				<h4 class="modal-title" id="myModalLabel">Configuration</h4>
 			</div>
+			
+			<!-- Modal Body -->
 			<div class="modal-body">
 				...
 			</div>
+			
+			<!-- Modal Footer -->
 			<div class="modal-footer">
 				<button type="button" class="btn btn-primary">Valider</button>
 			</div>
