0edf6c7cd3549b33d9e34aa43661fdd037acc906
jiyuan312986471 <jiyuan1005@hotmail.com>
2015-06-18 10:11:30 +0200

    .

--- dashboard/divs/menu.php
+++ dashboard/divs/menu.php
@@ -1,46 +1,40 @@
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                       
-                        <li>
-                            <a class="active" href="index.php"><i class="fa fa-dashboard fa-fw"></i> Accueil</a>
-                        </li>
-                    
-                        <li>
-                           <a href="machineAll.php"><i class="fa fa-table fa-fw" ></i> Machines</a>
-                                <ul class="nav nav-second-level">
-                            <li>
-                                    
-                                 <li>
-                                    <a href="machine.php?machine=AK">Final Auto K</a>
-                                </li>
-                                 <li>
-                                    <a href="machine.php?machine=SAK">Semi Auto K</a>
-                                </li>
-                                 <li>
-                                    <a href="machine.php?machine=DT1">Plateau TéSys D Taille 1</a>
-                                </li>
-                                 <li>
-                                    <a href="machine.php?machine=DT2">Plateau TéSys D Tailles 1 &  2</a>
-                                </li>
-                                 <li>
-                                    <a href="machine.php?machine=DT3">Plateau TéSys D Taille 3</a>
-                                </li>
-                                 <li>
-                                    <a href="machine.php?machine=SAD">Plateau TéSys D Semi auto</a>
-                                </li>            
-                               
-                                
-                            </ul>
-                        </li>
-                       
-                    
-                      
-                    </ul>
-                </div>
-             
-            </div>
-
-    
-
-          <!-- Menu -->
+<div class="navbar-default sidebar" role="navigation">
+	<div class="sidebar-nav navbar-collapse">
+		<ul class="nav" id="side-menu">
+			<li>
+				<a class="active" href="index.php">
+					<i class="fa fa-dashboard fa-fw"></i>
+					Accueil
+				</a>
+			</li>
+			<li>
+				<a href="machineAll.php">
+					<i class="fa fa-table fa-fw" ></i>
+					Machines
+				</a>
+				<ul class="nav nav-second-level">
+					<li>
+						<li>
+							<a href="machine.php?machine=AK">Final Auto K</a>
+						</li>
+						<li>
+							<a href="machine.php?machine=SAK">Semi Auto K</a>
+						</li>
+						<li>
+							<a href="machine.php?machine=DT1">Plateau TéSys D Taille 1</a>
+						</li>
+						<li>
+							<a href="machine.php?machine=DT2">Plateau TéSys D Tailles 1 &  2</a>
+						</li>
+						<li>
+							<a href="machine.php?machine=DT3">Plateau TéSys D Taille 3</a>
+						</li>
+						<li>
+							<a href="machine.php?machine=SAD">Plateau TéSys D Semi auto</a>
+						</li>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</div>
