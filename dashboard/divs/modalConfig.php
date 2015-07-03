78810cd8adb8e40ca29a3efc9251661f21c90b4b
jiyuan312986471 <jiyuan1005@hotmail.com>
2015-07-03 14:07:40 +0200

    .

--- dashboard/divs/modalConfig.php
+++ dashboard/divs/modalConfig.php
@@ -243,10 +243,11 @@
 													<div class="row" style="margin-left: -10px; margin-right: 0px; margin-bottom: 5px">
 														<div class="col-sm-offset-1 col-sm-7" style="padding: 0px">
 															<select id="selectTypeProduitNewMachine" class="form-control">
+																				<option value="empty">-- Choisissez le type produit --</option>
 																<?php foreach($listTypeProduit as $type){ ?>
-																				<option><?php echo $type; ?></option>
+																				<option value="<?php echo $type; ?>"><?php echo $type; ?></option>
 																<?php } ?>
-																				<option value="Nouveau Type">Nouveau Type...</option>
+																				<option value="NouveauType">Nouveau Type...</option>
 															</select>
 														</div>
 													</div>
