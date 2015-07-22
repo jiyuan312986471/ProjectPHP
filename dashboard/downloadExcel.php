9cbe9274597e20d417c400993c3d1a95aa4105f6
jiyuan312986471 <jiyuan1005@hotmail.com>
2015-07-22 15:58:06 +0200

    with phpexcel in index page

--- dashboard/downloadExcel.php
+++ dashboard/downloadExcel.php
@@ -1,15 +1,20 @@
 <?php
 
-	$path = $_GET['path'];
+	$fileName = $_GET['fileName'];
 	
-	header("Pragma: public");
-	header("Expires: 0");
-	header("Cache-Control:must-revalidate, post-check=0, pre-check=0");
+	$file = fopen("excel/test.xls", "r");
+	
+	//header("Pragma: public");
+	//header("Expires: 0");
+	//header("Cache-Control:must-revalidate, post-check=0, pre-check=0");
 	header("Content-Type:application/force-download");
-	header("Content-Type:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-	header("Content-Type:application/octet-stream");
-	header("Content-Type:application/download");;
-	header("Content-Disposition:attachment;filename=\"".$path."\"");
+	header("Content-Type:application/vnd.ms-excel");
+	//header("Content-Type:application/octet-stream");
+	//header("Content-Type:application/download");;
+	header("Content-Disposition:attachment;filename=\"".$fileName."\"");
 	header("Content-Transfer-Encoding:binary");
+	echo fread($file, filesize("excel/test.xls"));
+	
+	fclose($file);
 
 ?>
